61b5d15f10ecf91246bd4e62299034ad676e5f4e
hqyone <hqyone@gmail.com>
2015-02-02 02:06:33 -0700

    test commit file

--- test.py
+++ test.py
@@ -13,3 +13,14 @@ print ("hqyone")
 
 print ("xxxx")
 print (hex(34))
+
+myParams = {"server": 3}
+print ";".join(["%s=%s" % (k,v) for k,v in myParams.items()])
+
+def info(object, spacing=10, collapse=1):
+    methodList = [method for method in dir(object) if callable(getattr(object, method))]
+    procesFunc = collapse and (lambda s: " ".join(s.split())) or (lambda s:s)
+    print "\n".join(["%s %s" % (method.ljust(spacing), procesFunc(str(getattr(object, method).__doc__))) for method in methodList])
+
+if __name__ =="__main__":
+    print info.__doc__
